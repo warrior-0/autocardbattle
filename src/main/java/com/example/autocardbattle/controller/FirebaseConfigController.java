94e172dccbba09354bd0ba6f7236a3af18591802
warrior-0 <bodla0330@gmail.com>
2025-12-29 01:22:33 +0900

    Update FirebaseConfigController.java

--- src/main/java/com/example/autocardbattle/controller/FirebaseConfigController.java
+++ src/main/java/com/example/autocardbattle/controller/FirebaseConfigController.java
@@ -1,3 +1,5 @@
+package com.example.autocardbattle.controller;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
