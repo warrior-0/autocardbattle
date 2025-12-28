82360d537efb24562e66d72b7c599e82094ad34d
warrior-0 <bodla0330@gmail.com>
2025-12-28 22:39:17 +0900

    Update Monster.java

--- src/main/java/com/example/autocardbattle/model/Monster.java
+++ src/main/java/com/example/autocardbattle/model/Monster.java
@@ -1,31 +1,12 @@
 package com.example.autocardbattle.model;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
-import java.util.List;
-
+@Getter @AllArgsConstructor
 public class Monster {
     private String name;
-    private int maxHp;
     private int hp;
-    private List<Skill> skillChain;
-
-    public Monster(String name, int maxHp, List<Skill> skillChain) {
-        this.name = name;
-        this.maxHp = maxHp;
-        this.hp = maxHp;
-        this.skillChain = skillChain;
-    }
-
-    public void takeDamage(int dmg) {
-        hp -= dmg;
-        if (hp < 0) hp = 0;
-    }
-
-    public boolean isAlive() {
-        return hp > 0;
-    }
-
-    // Getter
-    public String getName() { return name; }
-    public int getHp() { return hp; }
-    public List<Skill> getSkillChain() { return skillChain; }
+    private int damage;
+    private int goldReward;
+    private int expReward;
 }
