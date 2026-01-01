770aec9931feb85204d8c37b012de4700fe1ce35
warrior-0 <bodla0330@gmail.com>
2026-01-02 02:09:55 +0900

    Update BattleService.java

--- src/main/java/com/example/autocardbattle/service/BattleService.java
+++ src/main/java/com/example/autocardbattle/service/BattleService.java
@@ -69,6 +69,9 @@ public class BattleService {
         state.readyUsers.clear();
         state.turn = 1; 
 
+        // [추가] 게임이 끝났으므로 서버 메모리에서 해당 방 정보 삭제
+        BattleController.removeRoomData(roomId);
+        
         return msg;
     }
 }
