c0bdcd42ffd135c4029e3d732eb114318f7939ee
warrior-0 <bodla0330@gmail.com>
2026-01-01 16:24:23 +0900

    Update UserRepository.java

--- src/main/java/com/example/autocardbattle/repository/UserRepository.java
+++ src/main/java/com/example/autocardbattle/repository/UserRepository.java
@@ -1,5 +1,4 @@
 package com.example.autocardbattle.repository;
-import com.example.autocardbattle.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, String> {
