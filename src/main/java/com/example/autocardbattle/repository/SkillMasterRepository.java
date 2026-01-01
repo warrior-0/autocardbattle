d4b5704e9188436beea9ad84717283e006066fe1
warrior-0 <bodla0330@gmail.com>
2026-01-01 13:04:49 +0900

    Delete src/main/java/com/example/autocardbattle/repository/SkillMasterRepository.java

--- src/main/java/com/example/autocardbattle/repository/SkillMasterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.autocardbattle.repository;
-
-import com.example.autocardbattle.entity.SkillMasterEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SkillMasterRepository extends JpaRepository<SkillMasterEntity, String> {
-}
