f100f0b5f9c1cab5242e51e05c151711bcdac0d7
warrior-0 <bodla0330@gmail.com>
2026-01-01 13:05:04 +0900

    Delete src/main/java/com/example/autocardbattle/repository/SkillChainRepository.java

--- src/main/java/com/example/autocardbattle/repository/SkillChainRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.autocardbattle.repository;
-import com.example.autocardbattle.entity.SkillChainEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface SkillChainRepository extends JpaRepository<SkillChainEntity, Long> {
-    List<SkillChainEntity> findByFirebaseUidOrderByChainOrderAsc(String firebaseUid);
-    void deleteByFirebaseUid(String firebaseUid);
-}
