eef39c4cf37e56f797ba18b81ace9c90496c958b
warrior-0 <bodla0330@gmail.com>
2026-01-01 23:31:39 +0900

    Create WebSocketConfig.java

---
+++ src/main/java/com/example/autocardbattle/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.example.autocardbattle.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic"); // 메시지를 받을 때 (구독)
+        config.setApplicationDestinationPrefixes("/app"); // 메시지를 보낼 때
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws") // 웹소켓 접속 주소: ws://.../ws
+                .setAllowedOriginPatterns("*")
+                .withSockJS(); // 낮은 버전 브라우저 지원
+    }
+}
