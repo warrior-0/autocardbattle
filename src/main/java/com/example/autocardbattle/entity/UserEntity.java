09f48d1ef52bb293ba14effa0c9f4e254d543c58
warrior-0 <bodla0330@gmail.com>
2026-01-01 17:06:57 +0900

    Create UserEntity.java

---
+++ src/main/java/com/example/autocardbattle/entity/UserEntity.java
@@ -0,0 +1,18 @@
+package com.example.autocardbattle.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@NoArgsConstructor
+public class UserEntity {
+    @Id
+    private String firebaseUid; // PK: Firebase에서 발급받은 고유 ID
+
+    @Column(unique = true, nullable = false)
+    private String username; // 유저 닉네임 (중복 불가)
+}
