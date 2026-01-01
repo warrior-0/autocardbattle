4064614ef8cdfbfe914f7a9c17cb312756bb0a3c
warrior-0 <bodla0330@gmail.com>
2026-01-01 21:46:58 +0900

    Update BattleResponse.java

--- src/main/java/com/example/autocardbattle/dto/BattleResponse.java
+++ src/main/java/com/example/autocardbattle/dto/BattleResponse.java
@@ -3,13 +3,14 @@ package com.example.autocardbattle.dto;
 import com.example.autocardbattle.entity.MapTileEntity;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Data;
 
-@NoArgsConstructor
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class BattleResponse {
-    private List<MapTileEntity> mapData; // 랜덤하게 선택된 맵 정보
-    private List<String> hand;           // 이번 턴에 배치할 주사위 2개 (타입명)
-    private int turn;                    // 현재 턴 (1~3)
+    private List<MapTileEntity> mapData;
+    private List<String> hand;
+    private int turn;
 }
