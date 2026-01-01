a3747c1512cdde66d8974becbb3a741b4d4e0217
warrior-0 <bodla0330@gmail.com>
2026-01-02 02:40:21 +0900

    Update WebSocketConfig.java

--- src/main/java/com/example/autocardbattle/config/WebSocketConfig.java
+++ src/main/java/com/example/autocardbattle/config/WebSocketConfig.java
@@ -20,4 +20,24 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .setAllowedOriginPatterns("*")
                 .withSockJS(); // 낮은 버전 브라우저 지원
     }
+    
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .addInterceptors(new HttpSessionHandshakeInterceptor() {
+                    @Override
+                    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, 
+                                                   WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+                        if (request instanceof ServletServerHttpRequest) {
+                            ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
+                            // URL 파라미터에서 userUid를 추출하여 웹소켓 세션에 저장
+                            String userUid = servletRequest.getServletRequest().getParameter("userUid");
+                            attributes.put("userUid", userUid);
+                        }
+                        return true;
+                    }
+                })
+                .withSockJS();
+    }
 }
