2370149bf721dedec1f48ae451b24dd95ddb08dd
warrior-0 <bodla0330@gmail.com>
2026-03-19 17:29:17 +0900

    Add GameSimulator class for game simulation

---
+++ src/main/resources/python/game_simulator.py
@@ -0,0 +1,16 @@
+class GameSimulator:
+    def __init__(self, players):
+        self.players = players
+
+    def simulate_game(self):
+        # Game simulation logic
+        pass
+
+    def run_simulation(self):
+        # Code to run the actual simulation
+        results = self.simulate_game()
+        return results
+
+# Example usage:
+# simulator = GameSimulator(['Player 1', 'Player 2'])
+# simulation_results = simulator.run_simulation()
