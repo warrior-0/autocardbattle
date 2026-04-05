a47355d209a097e1dea5308884003aea85e467fe
warrior-0 <bodla0330@gmail.com>
2026-04-05 12:38:47 +0900

    Update TrainingService.java

--- src/main/java/com/example/autocardbattle/service/TrainingService.java
+++ src/main/java/com/example/autocardbattle/service/TrainingService.java
@@ -29,6 +29,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class TrainingService {
